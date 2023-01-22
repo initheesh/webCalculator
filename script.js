ddfbba588f233e59fcbbc82bf7702b75da60a23c
i_nitheesh <nitheeshks07@gmail.com>
2023-01-23 00:05:29 +0530

    Fully working calculator without UI
    Implemented fully working calculator with help of eval() function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function btnClick(val){
-    console.log(val);
-    let op=['+','-','*','/'];
-    if(!op.includes(val))
         document.getElementById("output").value+=val;
+}
+function clearbtn(){
+    document.getElementById("output").value=""
+}
+
+function answer(){
+    let text =document.getElementById("output").value
+    console.log(text)
+    let result = eval(text);
+    document.getElementById("output").value=result
 }
 
